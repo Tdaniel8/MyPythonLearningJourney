f8b64347322c56e586ef96c66ebfc99b4fa37c31
Tdaniel <tosinolusola667@gmail.com>
2022-09-30 00:46:32 +0800

    added python number.py

---
+++ Python Numbers.py
@@ -0,0 +1,129 @@
+# INTEGER
+print (0b11011000) #Binary number printed as base10
+print (0b11000101)
+print (type(0b110100))
+
+print (0o1365227) #Octal number printed as base10
+print (0O1231157)
+print (type(0O132417))
+
+print(0x11675ae45)#Hexadecimal number printed as base10
+print(0X15abc4621)
+print(type(0x34ac1267))
+
+x = 1_234_567_890 #sep integer with underscore
+print (x)
+
+print (int(5.0)) #coverting float to integer
+print(int("1247")) #converting a string to integer
+
+
+# FLOAT
+f =1.2
+print(f)
+print (type(f))
+
+f =123_420.222_013 #sep float with underscore
+print(f)
+
+f =2e4 #representing numbers in terms of exponetial
+print (f)
+
+f =2e400 #representing numbers beyound float limit
+print (f)
+
+print (float(5)) #converting integer to float
+print (float("5.5")) #converting string to float
+print(float("        2"))
+print(float("1e3"))
+print(float("-Infinity"))
+print(float("inf"))
+
+
+print(hex(21))#Converting to base16
+print(hex(10))
+
+print(bin(12))#coverting to base2
+print(bin(20))
+
+print(oct(17))#converting to base8
+print(oct(24))
+
+print(abs(-12))#converting in number to it absolute
+print(abs(40))
+
+print(round(8.3))#round number to nearest number
+print(round(3.8))
+
+print(pow(3,2))#you returns power of 9
+print(pow(6,2))
+
+
+
+
+
+# OPERATORS
+
+print(2+2)#Addition
+print(10-20)#subtraction
+print(20/5)#Division
+print(20*10)#Multiplication
+print(9%2)#Modulo
+print(4**4)#Exponet/pow
+print(256//11)#FloorDivision
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
